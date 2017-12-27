066a4e327ea4079a5de58d04ffc2a0d250a3bef2
Joe Carlyon <joe.carlyon@uptake.com>
2017-12-27 11:29:18 -0600

    Added gitignore. Commented code. Updated README

--- README.md
+++ README.md
@@ -17,7 +17,8 @@ The **Paint Calculator** is a hypothetical project that calculates how many gall
 
 * Install Python / Pip
 * Run application
-* Write Selenium tests against application. They do **not** have to be in Python, and should be in whatever language you are most comfortable with.
-* You _are_ allowed to change any of the source code as you see fit to make things easier for yourself. 
+* Write Selenium tests against application. They do not have to be in Python, and should be in whatever language you are most comfortable with.
+* You are allowed to change any of the source code as you see fit to make things easier for yourself. You are encouraged to fix any bugs you discover.
 * Explain any problems you had while writing the tests, and what you did to make it easier. Pointing to localhost for the application is OK.
 * Would any of the tests written be suited for a lower level execution, or should they all be in Selenium? If so, where would they be better suited?
+
--- app/run.py
+++ app/run.py
@@ -6,17 +6,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Loads the index page
+    :return: Index page
+    """
     return render_template("index.html")
 
 
 @app.route('/dimensions', methods=['GET'])
 def dimensions():
+    """
+    Sanitizes inputs from the first page and displays the Dimensions page
+    :return: Dimensions page
+    """
     rooms = sanitize_input(request.args.get("rooms"))
     return render_template("dimensions.html", rooms=rooms)
 
 
 @app.route('/results', methods=['POST'])
 def results():
+    """
+    Performs most of the logic for paint calculations
+    :return: Results page
+    """
     data = request.values
     number_of_data_sets = len(data) / 3
     all_data = []
@@ -30,23 +42,43 @@ def results():
         total_gallons_required += calculate_gallons_required(formatted_data)
         all_data.append(formatted_data)
 
-    all_data
     return render_template("results.html", all_data=all_data, total_gallons_required=total_gallons_required)
 
 
 def calculate_feet(formatted_data):
+    """
+    Calculate the number of feet required to paint the surface area of a single room
+    :param formatted_data: dict of L/W/H information
+    :return: integer for the number of feet required by performing `((Length * 2) + (Width * 2)) * Height`
+    """
     return int(formatted_data['length']) * int(formatted_data['width']) * int(formatted_data['height'])
 
 
 def calculate_gallons_required(formatted_data):
-    return math.ceil(formatted_data['ft'] / 400)
+    """
+    Number of feet to paint divided by the amount of feet the paint will cover, rounded up
+    :param formatted_data: An integer for the number of feet required to paint
+    :return: feet / paint coverage, rounded up
+    """
+    return math.floor(formatted_data['ft'] / 350)
 
 
 def sanitize_input(input):
+    """
+    This universe doesn't allow for negative numbers of rooms or feet
+    :param input: Any number
+    :return: The absolute, integer number
+    """
     return abs(int(input))
 
 
 def extract_room_info(data, room_number):
+    """
+    Sanitizes inputs, and then constructs a dict of the room information
+    :param data: User input for LWH of a room
+    :param room_number: The number of the room that is being processed
+    :return: A formatted dict with room information
+    """
     formatted_data = {'length': sanitize_input(data.get("length-%d" % room_number)),
                       'width': sanitize_input(data.get("width-%d" % room_number)),
                       'height': sanitize_input(data.get("height-%d" % room_number))
@@ -54,5 +86,6 @@ def extract_room_info(data, room_number):
     return formatted_data
 
 
+# Boiler plate for starting the application
 if __name__ == '__main__':
     app.run()
